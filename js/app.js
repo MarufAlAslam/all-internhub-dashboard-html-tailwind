6cb111c38b7b991b7c9571adfdccf5bde6e53f2b
marufalaslam <maruf14@cse.pstu.ac.bd>
2024-02-14 16:10:35 +0600

    video added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,43 @@
+// $(".demo").videoBox({
+//   width: "100%",
+//   height: "360",
+//   loop: false,
+//   autoplay: true,
+//   muted: true,
+//   byline: true,
+//   color: "00adef",
+//   maxheight: "",
+//   maxwidth: "",
+//   portrait: true,
+//   title: "",
+// });
+
+$(".youtube-link").click(function () {
+  const btnContentText = $(this).text();
+  localStorage.setItem("videoLink", btnContentText);
+
+  //   reload the page
+  location.reload();
+});
+
+const videoLink = localStorage.getItem("videoLink");
+//   add new demo attribute
+$(".demo").attr("data-youtube", videoLink);
+
+$(".demo").videoBox({
+  width: "100%",
+  height: "360",
+  loop: false,
+  autoplay: true,
+  muted: true,
+  byline: true,
+  color: "00adef",
+  maxheight: "",
+  maxwidth: "",
+  portrait: true,
+  title: "",
+});
+
 const paymentForm = document.getElementById("payment-form");
 const paymentModal = document.getElementById("payment-modal");
 
